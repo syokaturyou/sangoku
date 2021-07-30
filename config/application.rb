25f836dd22ef92971706df727818772239cf405b
syokaturyou <teamsumagaku2@gmail.com>
2021-07-30 09:18:21 +0000

    <br>修正

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module Sangoku
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+
   end
 end
