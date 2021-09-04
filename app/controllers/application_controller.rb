29822b38231eee7dd68bb02279adb29a00bd1a3c
syokaturyou <teamsumagaku2@gmail.com>
2021-09-04 08:25:53 +0000

    フッダー編集 ©追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,18 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "sub-layout"
+    else
+      "application"
+    end
+  end
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email] )
     end
