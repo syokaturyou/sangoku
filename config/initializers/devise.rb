84619df308e3930063b6bfe0686accba5075d040
syokaturyou <teamsumagaku2@gmail.com>
2021-11-09 08:37:49 +0000

    Twitter認証更新

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -279,9 +279,11 @@ Devise.setup do |config|
   #                 scope: 'email',
   #                 info_fields: 'email,name'
 
-  config.omniauth :twitter,
-                  Settings.twitter[:twitter_api_key],
-                  Settings.twitter[:twitter_api_secret]
+  # config.omniauth :twitter,
+  #                 Settings.twitter[:twitter_api_key],
+  #                 Settings.twitter[:twitter_api_secret]
+
+  config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], scope: 'email', oauth_callback: "#{ENV['DOMAIN_NAME']}/members/auth/twitter/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
