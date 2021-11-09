aaa3196b8dc83722d59ac580984c812609ad5a71
syokaturyou <teamsumagaku2@gmail.com>
2021-11-09 06:38:13 +0000

    Twitter認証

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET'],
+  config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'],
   scope: 'email', oauth_callback: "#{ENV['DOMAIN_NAME']}/members/auth/twitter/callback"
   OmniAuth.config.logger = Rails.logger if Rails.env.development? # debug用
 
