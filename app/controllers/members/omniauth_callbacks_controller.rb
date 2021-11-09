f1415fbfbd6f0044378568ede2006c02f3f6cc5d
syokaturyou <teamsumagaku2@gmail.com>
2021-11-09 07:43:55 +0000

    ツイッター認証

--- app/controllers/members/omniauth_callbacks_controller.rb
+++ app/controllers/members/omniauth_callbacks_controller.rb
@@ -31,21 +31,23 @@ class Members::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   callback_from :twitter
   end
 
+  def failure
+    redirect_to new_member_registration_url
+  end
+
   private
   # コールバック時に行う処理
   def callback_from(provider)
-      provider = provider.to_s
+    provider = provider.to_s
+    @member = Member.find_or_create_from_auth(request.env['omniauth.auth'].except('extra'))
 
-    @member = Member.find_for_oauth(request.env['omniauth.auth'])
-
-    # persisted?でDBに保存済みかどうか判断
-    if @Member.persisted?
-      # サインアップ時に行いたい処理があればここに書きます。
+    if @member.persisted? # DBに保存済みかどうかを判定
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @member, event: :authentication
     else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to new_member_registration_url
+      session["devise.#{provider}_data"] = request.env['omniauth.auth'].except('extra')
+      redirect_to new_member_session_url
     end
   end
+
 end
