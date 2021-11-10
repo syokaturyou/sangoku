a25984bb51d32e4e6f83d008f5989df793b87bdd
syokaturyou <teamsumagaku2@gmail.com>
2021-11-10 06:59:02 +0000

    migrate更新

--- db/migrate/20211109053628_add_columns_to_members.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToMembers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :members, :uid, :string
-    add_column :members, :provider, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_062336) do
+ActiveRecord::Schema.define(version: 2021_11_10_065142) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
