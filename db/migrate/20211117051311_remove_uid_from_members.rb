b841db82718ee081e22c6139717a008ffc1558e2
syokaturyou <teamsumagaku2@gmail.com>
2021-11-17 05:38:09 +0000

    更新

--- db/migrate/20211117051311_remove_uid_from_members.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUidFromMembers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :members, :uid, :string
-  end
-end
--- db/migrate/20211117051319_remove_provider_from_members.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProviderFromMembers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :members, :provider, :string
-  end
-end
