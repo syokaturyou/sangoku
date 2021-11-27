13c567abb105d9c0d1c6061d798267968eda76da
syokaturyou <teamsumagaku2@gmail.com>
2021-11-27 05:04:53 +0000

    管理者側パンくずリスト追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,5 +1,15 @@
 crumb :root do
-  link "Home", root_path
+  link "会員一覧", manager_members_path
+end
+
+crumb :managermenbershow do
+  link "会員詳細", manager_member_path 
+  parent :root
+end
+
+crumb :managermenberedit do
+  link "会員情報編集", edit_manager_member_path
+  parent :managermenbershow
 end
 
 # crumb :projects do
