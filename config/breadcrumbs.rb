495ef951586991aecab8ace907b95f5b79873d90
syokaturyou <teamsumagaku2@gmail.com>
2021-11-27 05:47:27 +0000

    不要か所削除

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -16,25 +16,6 @@ crumb :managermenberedit do
   parent :managermenbershow
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
-
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
 # If you want to split your breadcrumbs configuration over multiple files, you
 # can create a folder named `config/breadcrumbs` and put your configuration
 # files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
