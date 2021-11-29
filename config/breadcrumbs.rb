0556248302ed17f82df82ecef70921330fd7dccc
syokaturyou <teamsumagaku2@gmail.com>
2021-11-29 06:43:26 +0000

    コメント追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -11,9 +11,3 @@ crumb :managermenberedit do
   link "会員情報編集", edit_manager_member_path
   parent :managermenbershow
 end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
