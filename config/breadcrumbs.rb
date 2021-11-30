e3aa59c8552784dae514ff1be9be6d63f02a9e5f
syokaturyou <teamsumagaku2@gmail.com>
2021-11-30 07:56:08 +0000

    トップページ 管理者側パンくずリスト更新

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,13 +1,33 @@
 crumb :root do
-  link "会員一覧", manager_members_path
+  link "トップページ", root_path
 end
 
-crumb :managermenbershow do
-  link "会員詳細", manager_member_path
+crumb :managermemberindex do
+  link "会員一覧", manager_members_path
   parent :root
 end
 
-crumb :managermenberedit do
+crumb :managermembershow do
+  link "会員詳細", manager_member_path
+  parent :managermemberindex
+end
+
+crumb :managermemberedit do
   link "会員情報編集", edit_manager_member_path
-  parent :managermenbershow
+  parent :managermembershow
+end
+
+crumb :articleindex do
+  link "お知らせ一覧", articles_path
+  parent :root
+end
+
+crumb :articleedit do
+  link "お知らせ編集", edit_article_path
+  parent :articleindex
+end
+
+crumb :articlenew do
+  link "お知らせ新規作成", new_article_path
+  parent :articleindex
 end
