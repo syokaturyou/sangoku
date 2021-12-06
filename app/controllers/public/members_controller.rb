5a2144bea20c90a7335969101427c0fbc92c67e4
syokaturyou <teamsumagaku2@gmail.com>
2021-12-06 14:20:26 +0000

    もとに戻す

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -8,7 +8,7 @@ class Public::MembersController < ApplicationController
     @member = Member.find(params[:id])
     @range = params[:range] # 過去回答と過去質問選択時に場合分け
     @posts = @member.posts.order(updated_at: 'DESC').page(params[:page]).per(10)
-    # @answers = @member.answers.order(updated_at: 'DESC').page(params[:page]).per(10)
+    @answers = @member.answers.order(updated_at: 'DESC').page(params[:page]).per(10)
   end
 
   # ログインしたユーザーだけ見えるようにcurrent_memberとする
