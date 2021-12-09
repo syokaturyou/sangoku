17e07ed430f18ca9c26bdfeac38a8f98f02df399
syokaturyou <teamsumagaku2@gmail.com>
2021-12-09 03:34:43 +0000

    test

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -25,12 +25,14 @@ class Public::PostsController < ApplicationController
       @posts = Post.all.order(impressions_count: 'DESC').page(params[:page]).per(10)
     when "PV数の少ない順に"
       @posts = Post.all.order(impressions_count: 'ASC').page(params[:page]).per(10)
+    # when "回答数の多い順に"
+    #   @posts = Post.joins(:answers).order('count(member_id) desc').group("answers.post_id").page(params[:page]).per(10)
     when "回答数の多い順に"
       @posts = Post.joins(:answers).order('count(member_id) desc').group("answers.post_id").page(params[:page]).per(10)
     # when "回答数の少ない順に"
     #   @posts = Post.joins(:answers).order('count(member_id) asc').group(:post_id).page(params[:page]).per(10)
      when "回答数の少ない順に"
-      @posts = Post.joins(:answers).where.not(member_id: nil).group(:post_id).page(params[:page]).per(10)
+      @posts = Post.joins(:answers).where.not(member_id: nil).group("answers.post_id").page(params[:page]).per(10)
     end
   end
 
