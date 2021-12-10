8e0462514c606ceed00aa7c2567630639b35725b
syokaturyou <teamsumagaku2@gmail.com>
2021-12-10 03:09:17 +0000

    文法更新

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
   attachment :postimage
 
   # 質問一覧画面で回答数0を見つける際に使用
-  scope :recent, -> { left_joins(:answers).where(answers: { member_id: nil }).order(updated_at: 'DESC') }
+  scope :recent, -> { left_joins(:answers).where(answers: {member_id: nil}).order(updated_at: 'DESC') }
 
   # 検索方法分岐
   def self.looks(search, word)
