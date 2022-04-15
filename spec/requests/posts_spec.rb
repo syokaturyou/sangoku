dd48773e8d378d5446e46a236073a94fa15e097d
syokaturyou <teamsumagaku2@gmail.com>
2022-04-15 14:46:17 +0000

    rspec更新

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET public/posts#show' do
+  describe 'GET public/posts#index' do
     # let(:post) { create(:post) }
 
     before do
-      get public_post_path(post.id)
+      get public_posts_path
     end
 
     it 'httpリクエストが正常に返るかどうか' do
