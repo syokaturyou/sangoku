e399d23d0c9e5a4b4ebba2b4f5d2d9042ff5d2b2
syokaturyou <teamsumagaku2@gmail.com>
2022-04-16 11:16:41 +0000

    rspec更新中

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -11,5 +11,9 @@ RSpec.describe 'Posts', type: :request do
     it 'httpリクエストが正常に返るかどうか' do
       expect(response).to have_http_status(:success)
     end
+
+    it '質問タイトルが表示されているかどうか' do
+      expect(response.body).to include post.posttitle
+    end
   end
 end
