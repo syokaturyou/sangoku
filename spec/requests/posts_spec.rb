fe203187fe77e9fab1beccee29d2f22f1322d02f
syokaturyou <teamsumagaku2@gmail.com>
2022-04-27 14:41:16 +0000

    更新

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET public/posts#index' do
-    let(:post) { create(:post) }
+    # let(:post) { create(:post) }
+    let(:posts) { create_list(:post, 4) }
     # let(:image) { create(:image) }
 
     before do
@@ -15,7 +16,9 @@ RSpec.describe 'Posts', type: :request do
     end
 
     it '質問タイトルが表示されているかどうか' do
-      expect(response.body).to include post.posttitle
+      posts.all? do |post|
+        expect(response.body).to include post.posttitle
+      end
     end
   end
 end
