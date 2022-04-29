5548bdd77ee6e0575bd368a5327b34fb2706e45a
syokaturyou <teamsumagaku2@gmail.com>
2022-04-29 08:57:16 +0000

    rspec考え中

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET public/posts#index' do
     # let(:post) { create(:post) }
-    let(:posts) { create_list(:post, 4) }
+    # let(:posts) { create_list(:post, 4) }
     # let(:image) { create(:image) }
 
     before do
       # post.images << image
+      # posts.each { |post| post.images << create(:image) }
       get public_posts_path
     end
 
@@ -15,10 +16,10 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to have_http_status(:success)
     end
 
-    it '質問タイトルが表示されているかどうか' do
-      posts.all? do |post|
-        expect(response.body).to include post.posttitle
-      end
-    end
+    # it '質問タイトルが表示されているかどうか' do
+    #   posts.all? do |post|
+    #     expect(response.body).to include post.posttitle
+    #   end
+    # end
   end
 end
