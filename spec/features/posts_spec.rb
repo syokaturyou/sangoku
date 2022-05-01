108177649ae3d67e19a2c6a9394ce9d6ab671679
syokaturyou <teamsumagaku2@gmail.com>
2022-05-01 05:03:04 +0000

    aaa

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -4,7 +4,8 @@ RSpec.feature 'Poss', type: :feature do
   describe 'display post details' do
     # let(:post) { create(:post) }
     # let(:posts) { create_list(:post, 4) }
-    # let(:image) { create(:image) }
+    given(:posts) { create_list(:product, 4) }
+    given(:image) { create(:image) }
 
     background do
       post.images << image
@@ -13,13 +14,15 @@ RSpec.feature 'Poss', type: :feature do
     end
 
     scenario '投稿の詳細情報が適切なタグ内に存在すること' do
-      expect(page).to have_selector 'td', text: post.posttitle
-      expect(page).to have_selector 'td', text: post.name
-      expect(page).to have_selector '.page-title', text: post.name
+      posts.all? do |post|
+        expect(page).to have_selector 'td', text: post.posttitle
+        expect(page).to have_selector 'td', text: post.name
+        expect(page).to have_selector '.page-title', text: post.name
+      end
     end
 
     scenario '詳細ページに遷移できること' do
-      click_link post.posttitle.truncate(25)
+      click_link "#{post.posttitle}"
       expect(current_path).to eq public_post_path(post)
     end
 
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET public/posts#index' do
     # let(:post) { create(:post) }
-    # let(:posts) { create_list(:post, 4) }
+    let(:posts) { create_list(:post, 4) }
     # let(:image) { create(:image) }
 
     before do
-      # post.images << image
-      # posts.each { |post| post.images << create(:image) }
+      post.images << image
+      posts.each { |post| post.images << create(:image) }
       get public_posts_path
     end
 
@@ -18,7 +18,7 @@ RSpec.describe 'Posts', type: :request do
 
     it '質問タイトルが表示されているかどうか' do
       posts.all? do |post|
-        expect(response.body).to include post.posttitle.truncate(25)
+        expect(response.body).to include post.posttitle
       end
     end
   end
