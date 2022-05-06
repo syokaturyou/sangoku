7029405c8b6fc10a9525e6676a2cb62439412919
syokaturyou <teamsumagaku2@gmail.com>
2022-05-06 05:20:06 +0000

    更新

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,26 +1,30 @@
 class MapsController < ApplicationController
-    def index
-        @maps = Map.all
-        @map = Map.new
-    end
+  def index
+    @maps = Map.all
+    @map = Map.new
+  end
 
-    def create
-        map = Map.new(map_params)
-        if map.save
-            redirect_to :action => "index"
-        else
-            redirect_to :action => "index"
-        end
+  def create
+    map = Map.new(map_params)
+    if map.save
+      flash[:notice] = '成功'
+      redirect_to :action => 'index'
+    else
+      flash[:notice] = '失敗'
+      redirect_to :action => 'index'
     end
+  end
 
-    def destroy
-        map = Map.find(params[:id])
-        map.destroy
-        redirect_to action: :index
-    end
+  def destroy
+    map = Map.find(params[:id])
+    map.destroy
+    flash[:notice] = '削除しました'
+    redirect_to action: :index
+  end
 
-    private
-    def map_params
+  private
+
+  def map_params
     params.require(:map).permit(:address, :latitude, :longitude, :profile)
-    end
+  end
 end
