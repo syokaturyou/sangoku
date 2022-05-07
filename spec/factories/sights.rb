651273a773a05daf9b912cac125497b407c67d63
syokaturyou <teamsumagaku2@gmail.com>
2022-05-07 16:58:52 +0000

    不要記述不要ファイル削除

--- spec/factories/sights.rb
+++ spec/factories/sights.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :sight do
-    address { "MyString" }
-    profile { "MyString" }
+    address { 'MyString' }
+    profile { 'MyString' }
     latitude { 1.5 }
     longitude { 1.5 }
     member_id { 1 }
-    mapimage_id { "MyString" }
+    mapimage_id { 'MyString' }
   end
 end
