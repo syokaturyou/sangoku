9c5ff6f0b31d6e25e961d12378da06c6e5828c07
syokaturyou <teamsumagaku2@gmail.com>
2022-05-08 14:23:51 +0000

    現時点で保存

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,6 @@ class HomesController < ApplicationController
   # お知らせ一覧とマップ一覧を表示
   def top
     @articles = Article.all.order(updated_at: 'DESC').page(params[:page]).per(10)
-    @sights = Sight.all.order(updated_at: 'DESC').page(params[:page]).per(4)
+    @sights = Sight.all
   end
 end
