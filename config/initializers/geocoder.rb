ec9cc462836736989954a9c66857c901195d3aaf
syokaturyou <teamsumagaku2@gmail.com>
2022-05-13 08:03:55 +0000

    apikey隠す

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -6,8 +6,7 @@ Geocoder.configure(
   # ip_lookup: :maxmind,
 
   # to use an API key:
-  # api_key: ENV['GOOGLE_MAP_API_KEY']
-  api_key: 'AIzaSyDppB6FuRU5yxHlaFTPTKyqlAAfRMQv49k'
+  api_key: ENV['GOOGLE_API_KEY']
 
   # geocoding service request timeout, in seconds (default 3):
   # timeout: 5,
