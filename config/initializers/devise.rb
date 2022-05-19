fc557c2d32aabd23f566dec2003a1461a13d39bc
syokaturyou <teamsumagaku2@gmail.com>
2022-05-19 15:55:38 +0000

    a

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -319,6 +319,7 @@ Devise.setup do |config|
 
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
   config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET_KEY'], skip_jwt: true
+
   # config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_API_SECRET_ID'], skip_jwt: true
   # config.omniauth :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_API_SECRET_ID'], skip_jwt: true
 
