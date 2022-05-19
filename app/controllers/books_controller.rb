68b6f1623b034df85b3b88b0fef7db671a1a4aa5
syokaturyou <teamsumagaku2@gmail.com>
2022-05-19 05:30:54 +0000

    楽天検索更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,27 @@
 class BooksController < ApplicationController
   def search
     if params[:keyword]
-      @books = RakutenWebService::Books::Book.search(title: params[:keyword])
+      @books = []
+      @range = params[:range]
+      case @range
+      when 'キーワード'
+        books = RakutenWebService::Books::Book.search(title: params[:keyword])
+        books.each do |book|
+          if book.item_caption.include?("")
+           @books.push(book)
+          end
+        end
+      when '著者'
+        books = RakutenWebService::Books::Book.search(author: params[:keyword])
+        books.each do |book|
+          if book.author.include?("")
+           @books.push(book)
+          end
+        end
+      end
+      if @books.present?
+       @books = Kaminari.paginate_array(@books).page(params[:page]).per(10)
+      end
     end
   end
 end
