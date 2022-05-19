c03ca81c72bca72136b7c33ac752c418382308ce
syokaturyou <teamsumagaku2@gmail.com>
2022-05-19 06:41:11 +0000

    楽天ブック検索終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,16 @@
 class BooksController < ApplicationController
   def search
+    # 楽天ブックスから検索 検索結果を配列に挿入してpaginate表示させる
     if params[:keyword]
-      @counts = RakutenWebService::Books::Book.search(title: params[:keyword])
       @books = []
-      @range = params[:range]
-      case @range
-      when 'キーワード'
-        books = RakutenWebService::Books::Book.search(title: params[:keyword])
-        books.each do |book|
-          if book.item_caption.include?("")
-           @books.push(book)
-          end
-        end
-      when '著者'
-        books = RakutenWebService::Books::Book.search(author: params[:keyword])
-        books.each do |book|
-          if book.author.include?("")
-           @books.push(book)
-          end
+      books = RakutenWebService::Books::Book.search(title: params[:keyword])
+      books.each do |book|
+        if book.title.include?("")
+        @books.push(book)
+        elsif book.item_caption.include?("")
+        @books.push(book)
+        elsif book.author.include?("")
+        @books.push(book)
         end
       end
       if @books.present?
