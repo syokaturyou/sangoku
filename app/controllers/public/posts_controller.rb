a748e8cec8258172337a4138f529c60f44186ff0
syokaturyou <teamsumagaku2@gmail.com>
2022-06-01 03:06:49 +0000

    現ユーザーのみ更新

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -41,6 +41,10 @@ module Public
 
     def edit
       @post = Post.find(params[:id])
+      # 現ユーザーのみ更新できるようにする
+      unless @post.member == current_member
+        redirect_to  new_public_post_path
+      end
     end
 
     def new
