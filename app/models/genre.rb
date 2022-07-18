927baeff11cdb7dacce8d52be0fb65e3848c8396
syokaturyou <teamsumagaku2@gmail.com>
2022-07-18 08:46:45 +0000

    バリデーションエラー追加

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,7 @@
 class Genre < ApplicationRecord
   has_many :posts, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: {  message: '同じ名称を入力しないでください'  }
 
   # 検索方法分岐
   def self.looks(search, word)
