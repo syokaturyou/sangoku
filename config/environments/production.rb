5fb7f8ede567676763e65f96f62ffd02c64d113e
syokaturyou <teamsumagaku2@gmail.com>
2022-10-16 21:23:40 +0000

    コメント追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   config.logger = Logger.new("log/sangokulog/sangoku.log", 5, 10 * 1024 * 1024)
+  # 日付ごとアップデート
   config.logger = Logger.update('log/sangokulog/sangoku.log', 'daily')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
